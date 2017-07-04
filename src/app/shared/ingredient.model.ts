532b471ae126ec0668f7854ba10527b07eca345a
Stephanie Nemeth <traumverloren@gmail.com>
2017-07-04 14:05:09 +0200

    more scaffolding of components

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+  // will give use these properties specified
+  // and automatically assign the values we receive to these properties
+  constructor(public name: string, public amount: number) {}
+}
